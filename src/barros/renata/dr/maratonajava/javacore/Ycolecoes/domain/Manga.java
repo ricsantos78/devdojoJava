8975b0b1b7ab6633bb791b6b6ccbe4600f46aced
Ricardo Santos <morpheu_jose@hotmail.com>
2022-06-03 19:47:46 -0300

    devdojojava

--- src/barros/renata/dr/maratonajava/javacore/Ycolecoes/domain/Manga.java
+++ src/barros/renata/dr/maratonajava/javacore/Ycolecoes/domain/Manga.java
@@ -7,6 +7,8 @@ public class Manga implements Comparable<Manga> {
     private String nome;
     private double preco;
 
+    private int quantidade;
+
     public Manga(Long id, String nome, double preco) {
         Objects.requireNonNull(id);
         Objects.requireNonNull(nome);
@@ -15,12 +17,18 @@ public class Manga implements Comparable<Manga> {
         this.preco = preco;
     }
 
+    public Manga(Long id, String nome, double preco, int quantidade) {
+        this(id, nome, preco);
+        this.quantidade = quantidade;
+    }
+
     @Override
     public String toString() {
         return "Manga{" +
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", preco=" + preco +
+                ", quantidade=" + quantidade +
                 '}';
     }
 
@@ -29,12 +37,20 @@ public class Manga implements Comparable<Manga> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Manga manga = (Manga) o;
-        return Double.compare(manga.preco, preco) == 0 && Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome);
+        return Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nome, preco);
+        return Objects.hash(id, nome);
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 
     public Long getId() {
